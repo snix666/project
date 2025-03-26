a0cb465fb122d91460d363b1a71224e25804212d
snix666 <viktor1522002@gmail.com>
2025-03-26 13:11:49 +0300

    Delete project-main directory

--- project-main/scripts/script.js
+++
@@ -1,9 +0,0 @@
-'use strict'
-document.addEventListener("DOMContentLoaded", () => {
-    console.log('Скрипт отработал корректно')
-});
-
-const intensiveImg = document.querySelector(".header__login");
-intensiveImg.addEventListener('click', () => {
-    console.log('всплывает окно регистрации');
-});
